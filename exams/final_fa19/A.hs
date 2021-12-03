91afe250355b5e639214e4bb6211037a252a9d9c
Owen Arden <owenarden@gmail.com>
2021-12-03 12:11:41 -0800

    edits

--- exams/final_fa19/A.hs
+++ exams/final_fa19/A.hs
@@ -2,7 +2,12 @@ f :: (a -> b) -> b -> [a] -> b
 f = undefined
 g :: (c -> Bool) -> a -> [c] -> [c]
 g = undefined
-h :: (c -> Bool) -> a -> [c] -> [c]
-h = f
+-- h :: (c -> Bool) -> a -> [c] -> [c]
+-- h = f
 
-main = undefined
+foo a b = let c = \a -> b ++ a in
+            let b = a ++ (c a) in 
+              c (bar b)
+  where bar e = e ++ a ++ b
+
+main = putStrLn $ foo "x" "o"
